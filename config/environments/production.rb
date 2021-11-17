bb4193fc52a1ad3a917b51ff1992f245aa0de49a
ryoma.takahashigit config --global user.email takahashiryouma0102@gmail.com <ubuntu@ip-172-31-30-8.ap-northeast-1.compute.internal>
2021-11-17 14:21:00 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-　　config.assets.compile = false
+　config.assets.compile = true
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "tasklist_production"
