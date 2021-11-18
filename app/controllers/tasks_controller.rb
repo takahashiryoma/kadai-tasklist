520b33a1b9ae6f4e40ef73343b72a9522816cc43
ryoma.takahashigit config --global user.email takahashiryouma0102@gmail.com <ubuntu@ip-172-31-30-8.ap-northeast-1.compute.internal>
2021-11-18 10:09:58 +0000

    put on :edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show, :destroy]
+  before_action :correct_user, only: [:show,:edit,:destroy]
 
  
   def index
