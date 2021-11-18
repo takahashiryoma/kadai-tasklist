91b9e773526feb55180fd39b33006e674c292cf4
ryoma.takahashigit config --global user.email takahashiryouma0102@gmail.com <ubuntu@ip-172-31-30-8.ap-northeast-1.compute.internal>
2021-11-18 11:09:37 +0000

    edit correct_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show,:new,:create,:edit,:update, :destroy]
+  before_action :correct_user, only: [:show,:edit,:update, :destroy]
 
  
   def index
@@ -9,7 +9,7 @@ class TasksController < ApplicationController
 
 
   def show
-    @task = Task.find(params[:id])
+    
   end
 
   def new
@@ -29,11 +29,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    
   end
 
   def update
-    @task = Task.find(params[:id])
+    
 
     if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
@@ -45,7 +45,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    
     @task.destroy
 
     flash[:success] = 'Taskは正常に削除されました'
